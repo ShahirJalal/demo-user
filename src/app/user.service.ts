c70eb0619a93d2a7644bb9365d3bd677e8f8781f
Shahir Jalal <shahirjalalhazli@gmail.com>
2024-04-25 01:53:48 +0800

    Added interface and service

---
+++ src/app/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private baseUrl = 'https://shahir-demo-4801d061dfee.herokuapp.com/api/users';
+
+  constructor(private http: HttpClient) { }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}`, user);
+  }
+
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.baseUrl}`);
+  }
+
+  getUserById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/${id}`);
+  }
+
+  updateUser(id: number, user: User): Observable<User> {
+    return this.http.put<User>(`${this.baseUrl}/${id}`, user);
+  }
+
+  deleteUser(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
